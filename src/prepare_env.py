8876f921183e191a8345698ba6773207ad71eb2f
Maxim Degi <unliex@yandex.ru>
2026-05-04 23:02:31 +0300

    added frame stacking

--- src/prepare_env.py
+++ src/prepare_env.py
@@ -2,13 +2,12 @@ from ray.rllib.env.wrappers.pettingzoo_env import ParallelPettingZooEnv
 from ray.tune.registry import register_env
 
 from custom_environment.custom_environment_v0 import CustomEnvironment
-
-
-# TODO: frame_stack_v1 isn't default???
+from supersuit import frame_stack_v1
 
 
 def _make_rllib_env(config):
     base = CustomEnvironment()
+    base = frame_stack_v1(base)
     wrapped = ParallelPettingZooEnv(base)
     wrapped._agent_ids = set(getattr(base, "possible_agents", []))
     return wrapped
