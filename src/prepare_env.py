b05d37457cd47e3e51755778c6dbbe8dd0f78177
Maxim Degi <unliex@yandex.ru>
2026-05-05 08:32:01 +0300

    bruh forgot to upload file

--- src/prepare_env.py
+++ src/prepare_env.py
@@ -2,12 +2,10 @@ from ray.rllib.env.wrappers.pettingzoo_env import ParallelPettingZooEnv
 from ray.tune.registry import register_env
 
 from custom_environment.custom_environment_v0 import CustomEnvironment
-from supersuit import frame_stack_v1
 
 
 def _make_rllib_env(config):
     base = CustomEnvironment()
-    base = frame_stack_v1(base)
     wrapped = ParallelPettingZooEnv(base)
     wrapped._agent_ids = set(getattr(base, "possible_agents", []))
     return wrapped
