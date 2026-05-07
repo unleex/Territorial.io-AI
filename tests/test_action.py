2b37c67fdc35f84a6a290056c90e389f35f7750b
Maxim Degi <unliex@yandex.ru>
2026-05-07 11:00:23 +0300

    updated test_action

--- tests/test_action.py
+++ tests/test_action.py
@@ -1,20 +1,20 @@
 from environment import CustomEnvironment
 from game.gameFuncs import findNeighbours
-import time
 
-env = CustomEnvironment()
+env = CustomEnvironment(rendering=True)
 env.step({0: [0, 10]})
 assert env.game.id_to_country[0].size > 0
 print("Neutral territory is attacked. Passed")
-env = CustomEnvironment()
+env.reset()
 neigh = findNeighbours(env.game, 0)
-while -1 in neigh:
-    env.step({0: [0, 10]})
+while not neigh or len(neigh) == 1 and -1 in neigh:
+    env.step({0: [env.permute_id(-1), 10]})
     neigh = findNeighbours(env.game, 0)
-neighbor = list(neigh.items())[0][0]
+neighbors = list(neigh.keys())
+if -1 in neighbors:
+    neighbors.remove(-1)
+neighbor = int(neighbors[0])
 old_neigh_size = env.game.id_to_country[neighbor].size
-while env.agents:
-    env.step({0: [env.permute_id(neighbor), 10]})
-    env.render()
-    time.sleep(0.05)
+env.step({0: [env.permute_id(neighbor), 10]})
 assert env.game.id_to_country[neighbor].size < old_neigh_size
+print("Enemy territory is attacked. Passed")
