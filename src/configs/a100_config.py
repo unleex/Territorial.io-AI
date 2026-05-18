a89f30ad0ce8c5d88252b8bbf748b0ec207f5565
Maxim Degi <unliex@yandex.ru>
2026-05-18 23:24:37 +0300

    adjusted a100 config

--- src/configs/a100_config.py
+++ src/configs/a100_config.py
@@ -11,8 +11,8 @@ config = (
         disable_env_checking=False,
     )
     .training(
-        train_batch_size=4096,
-        lr=2e-5,
+        train_batch_size=32_768 // 8,
+        lr=5e-5,
         gamma=0.99,
         lambda_=0.9,
         use_gae=True,
@@ -20,15 +20,15 @@ config = (
         grad_clip=0.5,
         entropy_coeff=0.1,
         vf_loss_coeff=0.25,
-        minibatch_size=4096,
-        num_epochs=30,
+        minibatch_size=8192 // 2,
+        num_epochs=15,
         model={"custom_model": MODEL_NAME},
     )
     .multi_agent(
         policies={"p0"},
         policy_mapping_fn=(lambda aid, *args, **kwargs: "p0"),
     )
-    .debugging(log_level="DEBUG")
+    .debugging(log_level="INFO")
     .framework(framework="torch")
     .api_stack(
         enable_rl_module_and_learner=False,
@@ -37,9 +37,12 @@ config = (
     .callbacks(VideoCallback)
     .resources(num_gpus=1)
     .env_runners(
-        num_env_runners=16, num_envs_per_env_runner=4, num_cpus_per_env_runner=1
+        num_env_runners=8, num_envs_per_env_runner=1, num_cpus_per_env_runner=2
     )
-    .learners(num_gpus_per_learner=1, num_learners=1)
+    .learners(
+        num_gpus_per_learner=1,
+        num_learners=1,
+    )
 )
-NUM_CPUS = 10
+NUM_CPUS = 20
 NUM_GPUS = 1
