bb61c7398156cb627a0a2c2846cbdbcd28d812c5
Maxim Degi <unliex@yandex.ru>
2026-05-19 10:38:22 +0300

    thus removed bleeding

--- src/environment.py
+++ src/environment.py
@@ -271,7 +271,6 @@ class CustomEnvironment(ParallelEnv):
                 / (self.game.n_grid_rows * self.game.n_grid_columns)
                 / 1000
             )
-            rewards[agent] -= 0.001
             terminations[agent] = not is_alive or won
             truncations[agent] = is_timeout if is_alive else False
             infos[agent] = {}
