8b78e4ac876659902c0c105dd930c290d0e6ffd2
Maxim Degi <unliex@yandex.ru>
2026-05-23 12:00:17 +0300

    based h200 config on common config

--- src/configs/h200_config.py
+++ src/configs/h200_config.py
@@ -1,16 +1,10 @@
 from model import MODEL_NAME
 from prepare_env import ENV_NAME
-from log import VideoCallback
 from configs.common import config
 
 CONFIG_NAME = "H200"
 config = (
-    config.environment(
-        env=ENV_NAME,
-        clip_actions=True,
-        disable_env_checking=False,
-    )
-    .training(
+    config.training(
         train_batch_size=32_768 // 2,
         minibatch_size=8192 // 2,
         lr=5e-5,
@@ -24,22 +18,10 @@ config = (
         num_epochs=15,
         model={"custom_model": MODEL_NAME},
     )
-    .multi_agent(
-        policies={"p0"},
-        policy_mapping_fn=(lambda aid, *args, **kwargs: "p0"),
-        count_steps_by="agent_steps",
-    )
-    .debugging(log_level="WARN")
-    .framework(framework="torch")
-    .api_stack(
-        enable_rl_module_and_learner=False,
-        enable_env_runner_and_connector_v2=False,
-    )
-    .callbacks(VideoCallback)
     .resources(num_gpus=1)
     .env_runners(
         num_env_runners=18,
-        num_envs_per_env_runner=14,
+        num_envs_per_env_runner=4,
         num_cpus_per_env_runner=1,
     )
     .learners(
